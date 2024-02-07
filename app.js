e1ef8dbad0dc377e3118ef80caaa237c78e825c7
Wilson-Tiquia <jwtiquia@gmail.com>
2024-02-07 21:54:57 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+
+/*--------------DECLARE THE ROUTERS HERE--------------*/
+
+
+
+const app = express();
+// just to check if we are in development or production you can comment code below
+console.log(process.env.NODE_ENV)
+// if in development show the logs of the api calls
+if (process.env.NODE_ENV === 'development') {
+
+    app.use(morgan('dev'))
+
+}
+
+app.get('/', (req,res)=>{
+    res.send("Hello from the server");
+})
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
+
+
+
+/*--------------DECLARE THE ROUTES HERE--------------*/
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+// configure the dotenv path
+dotenv.config({path: './config.env'});
+
+//connect to database
+const database = process.env.database.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose.connect(database, {
+    //useNewUrlParser: true,
+}).then(con=>{
+    //console.log(con.connection)
+    console.log("DB CONNECTION SUCCESS");
+});
+
+const app = require('./app');
+
+// start the server on port 3000
+const port = process.env.port;
+app.listen(port, (req,res)=>{
+
+    console.log(`Server running on port ${port}`);
+
+})
