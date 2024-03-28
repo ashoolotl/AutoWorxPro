78ab66ff5a392a40d83b292ade5ce86f347d3d05
Wilson-Tiquia <jwtiquia@gmail.com>
2024-03-29 06:08:55 +0800

    fixed database connectivity

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 // configure the dotenv path
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' });
 
 //connect to database
-const database = process.env.database.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
-mongoose.connect(database, {
-    //useNewUrlParser: true,
-}).then(con=>{
-    //console.log(con.connection)
-    console.log("DB CONNECTION SUCCESS");
-});
+const database = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+mongoose
+    .connect(database, {
+        //useNewUrlParser: true,
+    })
+    .then((con) => {
+        //console.log(con.connection)
+        console.log('DB CONNECTION SUCCESS');
+    });
 
 const app = require('./app');
 
 // start the server on port 3000
 const port = process.env.port;
-app.listen(port, (req,res)=>{
-
+app.listen(port, (req, res) => {
     console.log(`Server running on port ${port}`);
-
-})
+});
