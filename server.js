1fb797e110b321da5f55ef288229a9e2e58ab1cc
Wilson-Tiquia <jwtiquia@gmail.com>
2024-03-29 06:29:14 +0800

    fixed for deployment

--- server.js
+++ server.js
@@ -5,21 +5,24 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 //connect to database
-const database = process.env.DATABASE.replace(
+// const database = process.env.DATABASE.replace(
+//     '<PASSWORD>',
+//     process.env.DATABASE_PASSWORD
+// );
+// mongoose
+//     .connect(database, {
+//         //useNewUrlParser: true,
+//     })
+//     .then((con) => {
+//         //console.log(con.connection)
+//         console.log('DB CONNECTION SUCCESS');
+//     });
+
+const app = require('./app');
+const uri = process.env.DATABASE.replace(
     '<PASSWORD>',
     process.env.DATABASE_PASSWORD
 );
-mongoose
-    .connect(database, {
-        //useNewUrlParser: true,
-    })
-    .then((con) => {
-        //console.log(con.connection)
-        console.log('DB CONNECTION SUCCESS');
-    });
-
-const app = require('./app');
-const uri = process.env.DATABASE;
 const client = new MongoClient(uri);
 // start the server on port 3000
 const port = process.env.port;
