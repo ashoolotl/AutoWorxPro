d5c08ffa9ca4cb4d5ef2aea71bc7d86fe5f836e4
Wilson-Tiquia <jwtiquia@gmail.com>
2024-03-30 00:49:50 +0800

    test

--- app.js
+++ app.js
@@ -37,12 +37,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // LIMIT REQUEST FROM API
-const limiter = rateLimit({
-    max: 200,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too many request from this IP, please try again in an hour',
-});
-app.use('/api', limiter);
+// const limiter = rateLimit({
+//     max: 200,
+//     windowMs: 60 * 60 * 1000,
+//     message: 'Too many request from this IP, please try again in an hour',
+// });
+// app.use('/api', limiter);
 app.post(
     '/webhook-checkout',
     express.raw({ type: 'application/json' }),
