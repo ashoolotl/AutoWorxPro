1d948094f64b8cfad205a72010f3e7d633e82f18
Wilson-Tiquia <jwtiquia@gmail.com>
2024-03-30 02:38:29 +0800

    Fixed autthorization

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,12 +3,16 @@ const bookingController = require('../controllers/bookingController');
 const authController = require('../controllers/authController');
 const router = express.Router();
 router.use(authController.protect);
-router.use(authController.restrictTo('admin'));
+
 router.get(
     '/checkout-session/:userId',
     authController.protect,
     bookingController.getCheckoutSession
 );
-router.get('/', bookingController.getAllBookings);
+router.get(
+    '/',
+    authController.restrictTo('admin'),
+    bookingController.getAllBookings
+);
 
 module.exports = router;
